4348becbe10be8fd643ba2696a43d9b3305b1e68
rafaax <rafavfx1@gmail.com>
2024-04-16 10:04:48 -0300

    importando o calendario e fazendo ele ajustar a tela e selecionar a data no formato do banco de dados

--- application.py
+++ application.py
@@ -2,9 +2,14 @@ from tkinter import *
 from tkcalendar import Calendar
 import src.conn as conn
 import pymysql
+import datetime
+
+now = datetime.datetime.now()
+
 
 def grad_date():
-    date.config(text = "Selected Date is: " + cal.get_date())
+    data_formatada = datetime.datetime.strptime(cal.get_date(), "%m/%d/%y").strftime("%Y-%m-%d")
+    date.config(text = "Selected Date is: " + data_formatada)
 
 
 conn = pymysql.connect(host=conn.host, user=conn.user, password=conn.passw, database=conn.db,charset='utf8')
@@ -16,23 +21,22 @@ app.geometry('600x400')
 app.wm_maxsize(width=600, height=400)
 app.wm_minsize(width=600, height=400)
 
-cal = Calendar(app, selectmode = 'day',
-               year = 2020, month = 5,
-               day = 22)
-
-cal.pack(pady = 20)
-
-
-Button(app, text = "Get Date",
-       command = grad_date).pack(pady = 20)
-
-date = Label(app, text = "")
 
 rotulo = Label(app, text="Insira a placa")
 rotulo.pack()
 
 campo_texto = Entry(app, width=30)
 campo_texto.pack()
+
+cal = Calendar(app, selectmode = 'day',
+               year = now.year, month = now.month,
+               day = now.day)
+cal.pack(pady=10)
+
+Button(app, text = "Get Date", command = grad_date).pack()
+
+date = Label(app, text = "")
+date.pack()
 # query = 'SELECT placa, data_atualizacao, observacao, velocidade, pos_id, latitude, longitude FROM sau_posicionamento WHERE placa = "' + placa + '" AND date(data_atualizacao) = "' + data + '" ORDER BY data_atualizacao DESC'
 # cursor.execute(query)
 
