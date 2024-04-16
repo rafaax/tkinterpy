2119a0fe7f2b50cf1381315012df0c5c12cefdf4
rafaax <rafavfx1@gmail.com>
2024-04-16 08:44:12 -0300

    s1mple button

---
+++ simple_button.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def button_submit():
+    mostrar_mensagem()
+    mostrar_texto()
+
+def mostrar_mensagem():
+    messagebox.showinfo("Mensagem", "botao clicado.")
+
+
+def mostrar_texto():
+    texto_inserido = campo_texto.get()
+    if texto_inserido:
+        messagebox.showinfo("Texto Inserido", f"Você digitou: {texto_inserido}")
+    else:
+        messagebox.showwarning("Texto em branco", "Por favor, insira algum texto.")
+
+
+janela = tk.Tk()
+janela.title("Interface simples")
+
+
+rotulo = tk.Label(janela, text="Olá, Tkinter!")
+rotulo.pack()
+
+campo_texto = tk.Entry(janela, width=30)
+campo_texto.pack()
+
+
+
+botao = tk.Button(janela, text="Enviar", command=button_submit)
+botao.pack()
+
+janela.mainloop()
