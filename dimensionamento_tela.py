1ef0f5e9f9f851c9e9f0b59bb8e8b29fd018fc96
rafaax <rafavfx1@gmail.com>
2024-04-16 08:59:33 -0300

    trabalhando com dimensionamento de tela / minsize e maxsize

---
+++ dimensionamento_tela.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+app = Tk()
+app.title = 'Dimensionamento de tela'
+app.geometry('450x350')
+app.wm_maxsize(width=450, height=350)
+app.wm_minsize(width=225, height=175)
+
+app.mainloop()
