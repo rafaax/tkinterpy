0b17d625ee62f50777504e3607996fd4bfd30408
rafaax <rafavfx1@gmail.com>
2024-04-16 09:24:28 -0300

    ajustando titulo da application

---
+++ application.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+
+app = Tk()
+app.title('Gerar Relatorio')
+app.geometry('450x350')
+app.wm_maxsize(width=450, height=350)
+app.wm_minsize(width=225, height=175)
+
+
+
+app.mainloop()
--- examples/dimensionamento_tela.py
+++ examples/dimensionamento_tela.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 
 app = Tk()
-app.title = 'Dimensionamento de tela'
+app.title('Dimensionamento de tela')
 app.geometry('450x350')
 app.wm_maxsize(width=450, height=350)
 app.wm_minsize(width=225, height=175)
