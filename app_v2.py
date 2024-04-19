a22f27606eeb128565dfceef712bb099b05218ec
rafaax <rafavfx1@gmail.com>
2024-04-19 15:03:50 -0300

    app v2 ajustes

--- app_v2.py
+++ app_v2.py
@@ -202,30 +202,30 @@ def submit():
     
 
 
-def main():
-    now = datetime.datetime.now()
-    func = Functions()
-
-    app = Tk()
-    app.title('Gerar Relatorio')
-    app.geometry('300x150')
-    app.wm_maxsize(width=600, height=400)
-    app.wm_minsize(width=300, height=150)
-
-    text_1 = Label(app, text="Insira a placa")
-    text_1.pack()
-
-    input_text = Entry(app, width=12)
-    input_text.pack()
-
-    input_date = DateEntry(app, width=12, background='black', foreground='white', borderwidth=2, year=now.year, date_pattern="dd/mm/yyyy")
-    input_date.pack(pady=10)
-
-    Button(app, text= "Salvar", command=submit).pack()
-
-    app.mainloop()
 
 
-if __name__ == "__main__":
-    main()
+## main
+## interface inicial
 
+now = datetime.datetime.now()
+func = Functions()
+
+app = Tk()
+app.title('Gerar Relatorio')
+app.geometry('300x150')
+app.wm_maxsize(width=600, height=400)
+app.wm_minsize(width=300, height=150)
+
+text_1 = Label(app, text="Insira a placa")
+text_1.pack()
+
+input_text = Entry(app, width=12)
+input_text.pack()
+
+input_date = DateEntry(app, width=12, background='black', foreground='white', borderwidth=2, year=now.year, date_pattern="dd/mm/yyyy")
+input_date.pack(pady=10)
+
+Button(app, text= "Salvar", command=submit).pack()
+
+
+app.mainloop()
